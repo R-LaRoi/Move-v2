e6716caefae3bcab1d3c259f37328d895934ba8e
R. LaRoi <are@raewhoo.com>
2023-02-13 20:26:18 +0200

    Nav

---
+++ src/move/nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./nav.css";
+import imagegrid from "./imagegrid.png";
+
+export default function Nav() {
+  return (
+    <>
+      <nav>
+        <span className="wtm">what's the move</span>
+        <span className="rachel-link">
+          <a href="#about" style={{ textDecoration: "none" }}>
+            meet rachel
+          </a>
+        </span>
+      </nav>
+
+      <img src={imagegrid} alt="woman excercising" />
+      <div id="move"></div>
+    </>
+  );
+}
